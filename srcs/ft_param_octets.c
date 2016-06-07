9fcff9b9708a5cadd260ede28b9569f70aac647e
Chris <je.christophe.an@gmail.com>
2016-06-08 00:48:58 +0200

    08/06/16 00:48:58

--- srcs/ft_param_octets.c
+++ srcs/ft_param_octets.c
@@ -1,41 +1,17 @@
 #include "corewar.h"
 
+int 			ft_param_0_octet(t_vm *vm, t_player *plr, int octet, int index)
+{
+	return (0);
+}
+
 int 			ft_param_1_octets(t_vm *vm, t_player *plr, int octet, int index)
 {
-	return (plr->reg[vm->array[index].code_hexa]);
-}
+	int			reg;
 
-/*
-int 			ft_param_2_octets(t_vm *vm, t_player *plr, int octet, int index)
-{
-	int 		 		 			nb;
-	char 							*str;
-	char 							tab[9];
-	// short      						i;
-	int 							i;
-
-	ft_bzero(tab, sizeof(char) * 9);
-	nb = 0;
-	if (index > 4095)
-		index %= 4096;
-	if (octet == 1)
-		return (plr->reg[vm->array[index].code_hexa]);
-	while (octet-- > 0)
-	{
-		str = ft_itoa_base(vm->array[index].code_hexa, 16);
-		ft_strcat(tab, str);
-		if (vm->array[index].code_hexa)
-			nb = nb + ((ft_power(octet, 16)) * vm->array[index].code_hexa);
-		index++;
-		if (index > 4095)
-			index %= 4096;
-		ft_strdel(&str);
-	}
-	ft_atoi_base(tab, 16, &i);
-	i = (short)i;
-	return (i);
+	reg = vm->array[index].code_hexa;
+	return (reg);
 }
-*/
 
 int 			ft_param_4_octets(t_vm *vm, t_player *plr, int octet, int index)
 {
--- srcs/ft_processus.c
+++ srcs/ft_processus.c
@@ -3,7 +3,7 @@
 t_instruction g_instruction[] = {
 	{1, ft_live},
 	{2, ft_ld},
-	{3, ft_st},
+	// {3, ft_st},
 	{4, ft_add},
 	{5, ft_sub},
 	{6, ft_and},
@@ -18,14 +18,14 @@ t_instruction g_instruction[] = {
 };
 
 t_ocp g_ocp[] = {
-	{0, NULL},
+	{0, ft_param_0_octet},
 	{1, ft_param_1_octets},
 	{2, ft_param_4_octets},
 	{3, NULL},
 	{4, ft_param_4_octets},
 };
 
-static void 		ft_instruction_type(int tmp, int i, int *size_param, int *tab)
+static void 		ft_instruction_type(int tmp, int i, int *size, int *size_param)
 {
 	if (tmp == 3)
 		tmp--;
@@ -33,151 +33,149 @@ static void 		ft_instruction_type(int tmp, int i, int *size_param, int *tab)
 		tmp = i;
 	if (tmp > 0)
 	{
-		*tab = tmp;
-		*size_param += tmp;
+		*size_param = tmp;
+		*size += tmp;
 	}
 }
 
-static int			ft_ocp_instruction(unsigned char str, int i, int *tab)
+static int			ft_ocp_instruction(unsigned char str, int i, t_info *info)
 {
 	unsigned char 	tmp;
-	int 	size_param;
+	int 	size;
 
-	size_param = 0;
+	size = 0;
 	tmp = str >> 6;
-	ft_instruction_type(tmp, i, &size_param, &tab[0]);
+	info->t_f_param = tmp;
+	ft_instruction_type(tmp, i, &size, &info->s_f_param);
 	tmp = (str & MASK_6_BITS) >> 4;
-	ft_instruction_type(tmp, i, &size_param, &tab[1]);
+	info->t_s_param = tmp;
+	ft_instruction_type(tmp, i, &size, &info->s_s_param);
 	tmp = (str & MASK_4_BITS) >> 2;
-	ft_instruction_type(tmp, i, &size_param, &tab[2]);
-	return (size_param);
+	info->t_t_param = tmp;
+	ft_instruction_type(tmp, i, &size, &info->s_t_param);
+	return (size);
 }
+
+	// int 					tab[3];
+	// int 					i;
+
+	// ft_bzero(tab, sizeof(int) * 3);
+	// i = ft_check_size_max(1, plr->i_grid);
+	// tab[0] = ft_check_size_max(1, i);
+	// tab[1] = ft_check_size_max(2, i);
+	// tab[2] = ft_check_size_max(3, i);
+	// if (((tab[0] < 1 || tab[0] > 16) || (tab[1] < 1 || tab[1] > 16) || (tab[2] < 1 || tab[2] > 16)) && vm->array[i].code_hexa != 54)
+	// {
+	// 		ft_nothing(vm, plr);
+	// 		return (0);
+	// }
+	// plr->reg[tab[2]] = g_ocp[1].p(vm, plr, 1, tab[0]) + g_ocp[1].p(vm, plr, 1, tab[1]);
+	// plr->carry = (plr->reg[tab[2]]) ? 0 : 1; 
+	// plr->i_grid = ft_check_size_max(5, plr->i_grid);
+
+void 						ft_parse_info(t_vm *vm, t_player *plr, int octet)
+{
+	t_info					*tmp;
+
+	tmp = &plr->info;
+	tmp->index_ocp = (plr->i_grid + 1) % NB_CASE_TAB;
+	tmp->ocp = vm->array[tmp->index_ocp].code_hexa;
+	tmp->size_ocp_param = ft_ocp_instruction(tmp->ocp, octet, tmp);
+	tmp->index_f_param = (tmp->index_ocp + 1) % NB_CASE_TAB;
+	tmp->index_s_param = (tmp->index_ocp + tmp->s_f_param + 1) % NB_CASE_TAB;
+	tmp->index_t_param = (tmp->index_ocp + + tmp->s_f_param + tmp->s_s_param + 1) % NB_CASE_TAB;
+	tmp->nb_f_param = g_ocp[tmp->t_f_param].p(vm, plr, tmp->s_f_param, tmp->index_f_param);
+	tmp->nb_s_param = g_ocp[tmp->t_s_param].p(vm, plr, tmp->s_s_param, tmp->index_s_param);
+	tmp->nb_t_param = g_ocp[tmp->t_t_param].p(vm, plr, tmp->s_t_param, tmp->index_t_param);
+}
+
 
 int 						ft_add(t_vm *vm, t_player *plr)
 {
-	int 					tab[3];
-	int 					i;
+	ft_parse_info(vm, plr, 1);
 
-	ft_bzero(tab, sizeof(int) * 3);
-	i = ft_check_size_max(1, plr->i_grid);
-	tab[0] = ft_check_size_max(1, i);
-	tab[1] = ft_check_size_max(2, i);
-	tab[2] = ft_check_size_max(3, i);
-	if (((tab[0] < 1 || tab[0] > 16) || (tab[1] < 1 || tab[1] > 16) || (tab[2] < 1 || tab[2] > 16)) && vm->array[i].code_hexa != 54)
+	if (plr->info.nb_f_param < 1 || plr->info.nb_f_param > 16 ||
+			plr->info.nb_s_param < 1 || plr->info.nb_s_param > 16 ||
+				plr->info.nb_t_param < 1 || plr->info.nb_t_param > 16)
+		ft_nothing(vm, plr);
+	else
 	{
-			ft_nothing(vm, plr);
-			return (0);
+		plr->reg[plr->info.nb_t_param] = plr->reg[plr->info.nb_f_param] + plr->reg[plr->info.nb_s_param];
+		plr->carry = (plr->reg[plr->info.nb_t_param]) ? 0 : 1;
+		plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 	}
-	plr->reg[tab[2]] = g_ocp[1].p(vm, plr, 1, tab[0]) + g_ocp[1].p(vm, plr, 1, tab[1]);
-	plr->carry = (plr->reg[tab[2]]) ? 0 : 1; 
-	plr->i_grid = ft_check_size_max(5, plr->i_grid);
+	plr->do_instruction = 0;
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
 int 						ft_sub(t_vm *vm, t_player *plr)
 {
-	int 					tab[3];
-	int 					i;
+	ft_parse_info(vm, plr, 1);
 
-	ft_bzero(tab, sizeof(int) * 3);
-	i = ft_check_size_max(1, plr->i_grid);
-	tab[0] = ft_check_size_max(1, i);
-	tab[1] = ft_check_size_max(2, i);
-	tab[2] = ft_check_size_max(3, i);
-	if (((tab[0] < 1 || tab[0] > 16) || (tab[1] < 1 || tab[1] > 16) || (tab[2] < 1 || tab[2] > 16)) && vm->array[i].code_hexa != 54)
+	if (plr->info.nb_f_param < 1 || plr->info.nb_f_param > 16 ||
+			plr->info.nb_s_param < 1 || plr->info.nb_s_param > 16 ||
+				plr->info.nb_t_param < 1 || plr->info.nb_t_param > 16)
+		ft_nothing(vm, plr);
+	else
 	{
-			ft_nothing(vm, plr);
-			return (0);
+		plr->reg[plr->info.nb_t_param] = plr->reg[plr->info.nb_f_param] - plr->reg[plr->info.nb_s_param];
+		plr->carry = (plr->reg[plr->info.nb_t_param]) ? 0 : 1;
+		plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 	}
-	plr->reg[tab[2]] = g_ocp[1].p(vm, plr, 1, tab[0]) - g_ocp[1].p(vm, plr, 1, tab[1]);
-	plr->carry = (plr->reg[tab[2]]) ? 0 : 1; 
-	plr->i_grid = ft_check_size_max(5, plr->i_grid);
+	plr->do_instruction = 0;
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
 int 						ft_and(t_vm *vm, t_player *plr)
 {
-	int 					i;
-	int 					tmp_i;
-	int 					ocp;
-	int 					tab[3];
-	int 					tmp;
-
-	ft_bzero(tab, sizeof(int) * 3);
-	i = ft_check_size_max(1, plr->i_grid);
-	tmp = i;
-	ocp = ft_ocp_instruction(vm->array[i].code_hexa, 4, tab);
-	i = ft_check_size_max(1, i);
-	tmp_i = ft_check_size_max(tab[0] + tab[1], i);
-	if ((vm->array[tmp_i].code_hexa < 1 || vm->array[tmp_i].code_hexa > 16) && (vm->array[tmp].code_hexa != 84 && vm->array[tmp].code_hexa != 88 && vm->array[tmp].code_hexa != 116 && 
-		vm->array[tmp].code_hexa != 148 && vm->array[tmp].code_hexa != 164 && vm->array[tmp].code_hexa != 180 &&
-		vm->array[tmp].code_hexa != 212 && vm->array[tmp].code_hexa != 228 && vm->array[tmp].code_hexa != 244))
+	ft_parse_info(vm, plr, 4);
+	
+	if (plr->info.nb_t_param < 1 || plr->info.nb_t_param > 16)
+		ft_nothing(vm, plr);
+	else
 	{
-			ft_nothing(vm, plr);
-			return (0);
+		plr->reg[plr->info.nb_t_param] = plr->info.nb_f_param | plr->info.nb_s_param;
+		plr->carry = (plr->reg[plr->info.nb_t_param]) ? 0 : 1;
+		plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 	}
-	plr->reg[vm->array[tmp_i].code_hexa] = g_ocp[tab[0]].p(vm, plr, tab[0], i) & g_ocp[tab[1]].p(vm, plr, tab[1], i + tab[0]);
-	plr->carry = (plr->reg[vm->array[tmp_i].code_hexa]) ? 0 : 1; 
-	plr->i_grid = ft_check_size_max(ocp + 2, plr->i_grid);
 	plr->do_instruction = 0;
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
 int 						ft_or(t_vm *vm, t_player *plr)
 {
-	int 					i;
-	int 					tmp_i;
-	int 					ocp;
-	int 					tab[3];
-	int 					tmp;
-
-	ft_bzero(tab, sizeof(int) * 3);
-	i = ft_check_size_max(1, plr->i_grid);
-	tmp = i;
-	ocp = ft_ocp_instruction(vm->array[i].code_hexa, 4, tab);
-	i = ft_check_size_max(1, i);
-	tmp_i = ft_check_size_max(tab[0] + tab[1], i);
-	if ((vm->array[tmp_i].code_hexa < 1 || vm->array[tmp_i].code_hexa > 16) &&
-		(vm->array[tmp].code_hexa != 84 && vm->array[tmp].code_hexa != 88 && vm->array[tmp].code_hexa != 116 && 
-		vm->array[tmp].code_hexa != 148 && vm->array[tmp].code_hexa != 164 && vm->array[tmp].code_hexa != 180 &&
-		vm->array[tmp].code_hexa != 212 && vm->array[tmp].code_hexa != 228 && vm->array[tmp].code_hexa != 244))
+	ft_parse_info(vm, plr, 4);
+	
+	if (plr->info.nb_t_param < 1 || plr->info.nb_t_param > 16)
+		ft_nothing(vm, plr);
+	else
 	{
-			ft_nothing(vm, plr);
-			return (0);
+		plr->reg[plr->info.nb_t_param] = plr->info.nb_f_param | plr->info.nb_s_param;
+		plr->carry = (plr->reg[plr->info.nb_t_param]) ? 0 : 1;
+		plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 	}
-	plr->reg[vm->array[tmp_i].code_hexa] = g_ocp[tab[0]].p(vm, plr, tab[0], i) | g_ocp[tab[1]].p(vm, plr, tab[1], i + tab[0]);
-	plr->carry = (plr->reg[vm->array[tmp_i].code_hexa]) ? 0 : 1; 
-	plr->i_grid = ft_check_size_max(ocp + 2, plr->i_grid);
 	plr->do_instruction = 0;
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
 int 						ft_xor(t_vm *vm, t_player *plr)
 {
-	int 					i;
-	int 					tmp_i;
-	int 					ocp;
-	int 					tab[3];
-	int 					tmp;
-
-	ft_bzero(tab, sizeof(int) * 3);
-	i = ft_check_size_max(1, plr->i_grid);
-	tmp = i;
-	ocp = ft_ocp_instruction(vm->array[i].code_hexa, 4, tab);
-	i = ft_check_size_max(1, i);
-	tmp_i = ft_check_size_max(tab[0] + tab[1], i);
-	if ((vm->array[tmp_i].code_hexa < 1 || vm->array[tmp_i].code_hexa > 16) &&
-		(vm->array[tmp].code_hexa != 84 && vm->array[tmp].code_hexa != 88 && vm->array[tmp].code_hexa != 116 && 
-		vm->array[tmp].code_hexa != 148 && vm->array[tmp].code_hexa != 164 && vm->array[tmp].code_hexa != 180 &&
-		vm->array[tmp].code_hexa != 212 && vm->array[tmp].code_hexa != 228 && vm->array[tmp].code_hexa != 244))
+	ft_parse_info(vm, plr, 4);
+	
+	if (plr->info.nb_t_param < 1 || plr->info.nb_t_param > 16)
+		ft_nothing(vm, plr);
+	else
 	{
-			ft_nothing(vm, plr);
-			return (0);
+		plr->reg[plr->info.nb_t_param] = plr->info.nb_f_param | plr->info.nb_s_param;
+		plr->carry = (plr->reg[plr->info.nb_t_param]) ? 0 : 1;
+		plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 	}
-	plr->reg[vm->array[tmp_i].code_hexa] = g_ocp[tab[0]].p(vm, plr, tab[0], i) ^ g_ocp[tab[1]].p(vm, plr, tab[1], i + tab[0]);
-	plr->carry = (plr->reg[vm->array[tmp_i].code_hexa]) ? 0 : 1; 
-	plr->i_grid = ft_check_size_max(ocp + 2, plr->i_grid);
 	plr->do_instruction = 0;
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
@@ -203,84 +201,18 @@ int 						ft_nothing(t_vm *vm, t_player *plr)
 	return (0);
 }
 
-/*
-int 						ft_st(t_vm *vm, t_player *plr)
-{
-	int 					i;
-	int 					ocp;
-	int 					tab[4];
-	int 					nb_reg;
-
-	ft_bzero(tab, sizeof(int) * 4);
-	i = ft_check_size_max(1, plr->i_grid);
-	nb_reg = vm->array[i + 1 % NB_CASE_TAB].code_hexa;
-	ocp = ft_ocp_instruction(vm->array[i].code_hexa, 2, tab);
-	// if ((vm->array[i].code_hexa != 70 && vm->array[i].code_hexa != 50)
-	// 		|| (nb_reg < 1 || nb_reg > 17))
-	// {
-	// 	ft_nothing(vm,plr);
-	// 	return (0);
-	// }
-	// if (tab[1] == 1 && (vm->array[(i + tab[1]) % NB_CASE_TAB].code_hexa < 1 || vm->array[(i + tab[1]) % NB_CASE_TAB].code_hexa < 16))
-	// {		
-	// 	ft_nothing(vm,plr);
-	// 	return (0);
-	// }
-	if (tab[1] == 1)
-		plr->reg[vm->array[(i + 2) % NB_CASE_TAB].code_hexa] = plr->reg[nb_reg];
-	else
-		ft_print_param_to_array_4_octets(vm, plr, plr->i_grid + ft_param_4_octets(vm, plr, 2, i + 2), plr->reg[nb_reg]);
-	
-	plr->do_instruction = 0;
-	plr->i_grid = ft_check_size_max(ocp + 2, plr->i_grid);
-	return (0);
-}
-*/
-
 int 						ft_sti(t_vm *vm, t_player *plr)
 {
 	int 					i;
-	int 					ocp;
-	int 					tab[4];
-	int 					nb_reg;
+	ft_parse_info(vm, plr, 2);
 
-	ft_bzero(tab, sizeof(int) * 4);
-	i = ft_check_size_max(1, plr->i_grid);
-	nb_reg = vm->array[i + 1].code_hexa;
-
-	// printf("\n\n%d\n", nb_reg);
-	// if ((nb_reg < 1 || nb_reg > 16) && ((vm->array[i].code_hexa != 132 && vm->array[i].code_hexa != 136 &&
-	// 		vm->array[i].code_hexa != 100 && vm->array[i].code_hexa != 104 
-	// 		&& vm->array[i].code_hexa != 116 && vm->array[i].code_hexa != 120)))
-	// {
-	// 		ft_nothing(vm, plr);
-	// 		return (0);
-	// }
-	/*
-	** 2, car les directs dans cette instruction sont code sous 2 octets
-	*/
-
-	ocp = ft_ocp_instruction(vm->array[i].code_hexa, 2, tab);
-
-	/*
-	** j'incremete de 2, pour que l'index passe au second parametre de l'instruction.
-	** le premier etant forcement un registre.
-	*/
-
-	i = g_ocp[tab[1]].p(vm, plr, tab[1], i + 2) + g_ocp[tab[2]].p(vm, plr, tab[2], i + 2 + tab[1]);
-
-
+	i = plr->info.nb_s_param + plr->info.nb_t_param;
 	i = ft_check_size_max(i, plr->i_grid);
-	ft_print_param_to_array_4_octets(vm, plr, i, plr->reg[nb_reg]);
-
-
-	/*
-	** ocp = taile de l'instruction
-	** 2 = le nom de l'instruction + le code ocp
-	*/
+	ft_print_param_to_array_4_octets(vm, plr, i, plr->reg[plr->info.nb_f_param]);
+	plr->i_grid = (plr->i_grid + 2 + plr->info.size_ocp_param) % NB_CASE_TAB;
 
 	plr->do_instruction = 0;
-	plr->i_grid = ft_check_size_max(ocp + 2, plr->i_grid);
+	ft_bzero(&plr->info, sizeof(t_info));
 	return (0);
 }
 
