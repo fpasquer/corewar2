455180a9cbf64cd97a92f229e36597c8701d2994
Jean-christophe CHEN <jchen@e1r4p11.42.fr>
2016-06-15 00:20:51 +0200

    t

--- test.s
+++ test.s
@@ -1,13 +1,35 @@
 .name "test"
 .comment "qwe"
 
-ld  %11, r4
-ld 	%173277184, r5
-sti r1, %:live, %1
+ld %196557, r5
+ld %225282, r4
+#ld 	%192151552, r5
+#sti r1, %:live, %1
 
-live: live %1
-fork: fork %:live
-
+#live: live %1
+#fork: fork %:live
 st r4, 55
-st r5, 54
-st r5, -13
+st r5, 47
+#live: live %1
+
+#fork: fork %:live
+#live %1
+#st r5, -8
+
+#ld %189267968, r3
+#ld %185597952, r4
+#ld %187695104, r5
+#ld %186974208, r6
+#ld %188743680, r7
+#ld %185925632, r8
+#ld %188088320, r9
+#ld %186056704, r10
+
+#st r10, 300
+#st r9, 40
+#st r8, 50
+#st r7, 60
+#st r6, 70
+#st r5, 80
+#st r4, 90
+#st r3, 100
